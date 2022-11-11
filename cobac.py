5e2d2ba052295362931cfd078a4a5ba61cbb7e6c
ngmjnkdan <117984860+ngmjnkdan@users.noreply.github.com>
2022-11-11 18:37:46 +0700

    Create cobac.py

---
+++ cobac.py
@@ -0,0 +1,11 @@
+while True:
+	i = int(input("📍Số may mắn nằm trong từ 1 đến 1000:  "))
+	if i < 90:
+		print('Số bạn đoán hơi nhỏ,chọn số khác đi 🙏')
+	elif i < 100:
+		print('Đoán gần đúng rồi cố lên 🧠')
+	elif i = 100:
+		print("Chúc mừng bạn đã đoán trúng ✅")
+		break
+	else:
+		print('Bạn đoán chưa đúng rồi ❌')
